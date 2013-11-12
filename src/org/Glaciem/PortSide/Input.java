c2e289e423c7d1f7b52cbafa1cc7f9f588671334
Erik Wilson <tritania@thor.thor>
2013-11-12 00:39:11 -0400

    Fixed an issue where the game would not closed

--- src/org/Glaciem/PortSide/Input.java
+++ src/org/Glaciem/PortSide/Input.java
@@ -1,5 +1,16 @@
 package org.Glaciem.PortSide;
 
+import org.lwjgl.input.Keyboard;
+
 public class Input {
+	
+	public static void quit()
+	{
+		if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) 
+		{
+			PortSide.finished = true;
+		}
+		
+	}
 
 }

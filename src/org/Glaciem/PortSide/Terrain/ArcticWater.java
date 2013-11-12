1aaa790c818ae763d1a7f062c6e3af309bbddd1b
MaltaProject <nicholas.ingalls@gmail.com>
2013-11-12 01:54:30 -0400

    Arctic Water

--- src/org/Glaciem/PortSide/Terrain/ArcticWater.java
+++ src/org/Glaciem/PortSide/Terrain/ArcticWater.java
@@ -4,4 +4,16 @@ import org.Glaciem.PortSide.Terrain.Interface.Terrain;
 
 public class ArcticWater extends Water implements Terrain {
 
+	public int getHeight(){
+		return 0;
+	}
+	
+	public boolean isIce(){
+		return true;
+	}
+	
+	public TerrainType getTerrainType(){
+		return TerrainType.ARCTIC_WATER;
+	}
+	
 }
--- src/org/Glaciem/PortSide/Terrain/Beach.java
+++ src/org/Glaciem/PortSide/Terrain/Beach.java
@@ -3,5 +3,5 @@ package org.Glaciem.PortSide.Terrain;
 import org.Glaciem.PortSide.Terrain.Interface.Terrain;
 
 public class Beach extends Land implements Terrain {
-
+	
 }

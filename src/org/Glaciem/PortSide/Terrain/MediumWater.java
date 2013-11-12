099f373407f9f140d88983f68614f0cd6e325bdc
MaltaProject <nicholas.ingalls@gmail.com>
2013-11-12 02:02:50 -0400

    Deep Water

--- src/org/Glaciem/PortSide/Terrain/MediumWater.java
+++ src/org/Glaciem/PortSide/Terrain/MediumWater.java
@@ -3,5 +3,11 @@ package org.Glaciem.PortSide.Terrain;
 import org.Glaciem.PortSide.Terrain.Interface.Terrain;
 
 public class MediumWater extends Water implements Terrain{
-
+	public int getHeight(){
+		return -500;
+	}
+	
+	public TerrainType getTerrainType(){
+		return TerrainType.MEDIUM_WATER;
+	}
 }

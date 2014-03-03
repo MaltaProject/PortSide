cfc81476c976bf4305070bafb4953e47ea582e1e
Tritania <erikwilson@magnorum.com>
2014-03-03 18:32:23 -0400

    Added basic texturing and model support, edited camera placement.

--- js/canvas.js
+++ js/canvas.js
@@ -1,23 +1,32 @@
 function canvas() 
 {
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.5, 1000);
+        camera.position.x = 0;
+        camera.position.z = 4;
+        camera.position.y = 0;
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    var geometry = new THREE.PlaneGeometry(5, 5);
-    var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-    var square = new THREE.Mesh(geometry, material);
-    scene.add(square);
+    var texture    = new THREE.ImageUtils.loadTexture("data/textures/ship.png");
+    var jsonLoader = new THREE.JSONLoader();
 
-    camera.position.z = 5;
-
+    jsonLoader.load('data/models/ship.js', addModelToScene);        
     var render = function () {
         requestAnimationFrame(render);
         renderer.render(scene, camera);
     };
 
     render();
+    
+    function addModelToScene( geometry, materials ) 
+    {
+        var material = new THREE.MeshBasicMaterial({map:texture});
+        mesh = new THREE.Mesh( geometry, material );
+        scene.add( mesh );
+    }
 }
+
+

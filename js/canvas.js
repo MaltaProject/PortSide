0f116451a1919527ce8c29e1ef5f460413b7b534
Tritania <erikwilson@magnorum.com>
2014-03-11 21:29:07 -0300

    Added function to create new 3d objects easily, create a new file that will keep polygon count low.

--- js/canvas.js
+++ js/canvas.js
@@ -1,45 +1,64 @@
-    mesh = new THREE.Object3D();
     var clock = new THREE.Clock();
     var scene = new THREE.Scene();
     var keyboard = new THREEx.KeyboardState();
-    
+    var objects = [];
+
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.5, 1000);
-        camera.position.x = 0;
-        camera.position.z = 17;
-        camera.position.y = 0;
+        camera.position.set( 0, 0, 20);
+
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
     
     THREEx.WindowResize(renderer, camera);
-
-    var texture    = new THREE.ImageUtils.loadTexture("data/textures/ship.png");
-    var jsonLoader = new THREE.JSONLoader();
-
-    jsonLoader.load('data/models/ship.js', addModelToScene);        
-    var render = function () {
-        requestAnimationFrame(render);
-        renderer.render(scene, camera);
-    };
-
-    animate();
     
-    function addModelToScene( geometry, materials ) 
+    animate();
+
+    /*
+     * type  gives the type of model to be loaded
+     * location  gives the location to draw the model
+     * iff gives the team that the ship should be assigned to
+     */
+    function addModel(type, location, iff)
     {
-        var material = new THREE.MeshBasicMaterial({map:texture});
-        mesh = new THREE.Mesh( geometry, material );
-        scene.add( mesh );
+        var loader = new THREE.JSONLoader(); // init the loader util
+
+        // init loading
+        loader.load('data/models/'+type+'.js', function (geometry) {
+        // create a new material
+        var material = new THREE.MeshBasicMaterial({
+        map: THREE.ImageUtils.loadTexture('data/textures/'+type+'.png'),  // specify and load the texture
+        });
+  
+          // create a mesh with models geometry and material
+        var mesh = new THREE.Mesh(
+            geometry,
+            material
+        );
+          
+        
+          
+        scene.add(mesh);
+        objects.push(mesh);
+        });
+        
     }
     
     function animate() 
     {
         requestAnimationFrame( animate );
         render();
+        input();
         update();
     }
     
-    function update()
+    function render() 
+    {
+        renderer.render( scene, camera );
+    }
+    
+    function input()
     {
         var moveDistance = 10 * clock.getDelta(); 
 
@@ -54,6 +73,9 @@
             
         if ( keyboard.pressed("D") )
             mesh.translateX(  moveDistance );
+        
+        if ( keyboard.pressed("V") )
+            addModel("patrol",1,3);
     }
     
 
---
+++ js/update.js
@@ -0,0 +1,8 @@
+/*
+ * This function is used to keep polygon count low by removing polygons
+ * that do not appear on the screen.
+ */
+function update()
+{
+    
+}

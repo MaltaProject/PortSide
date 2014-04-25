4d0096e4a3932633a54f1f3578a467e53d40bf8e
Tritania <erikwilson@magnorum.com>
2014-04-25 00:46:22 -0300

    Removed old collision detection and began implementing new system.

--- js/portsideMath.js
+++ js/portsideMath.js
@@ -7,30 +7,10 @@ function pointDistance(x, y, xp, yp)
 	return distance;
 }
 
-function detectCollision(objects)
-{
-	for(var i = 0; i < objects.length; i++)
-	{
-		var objectsp = [];
-		for(var j = 0; j < objects.length-1; j++)
-		{
-			if(j != i)
-				objectsp.push(objects[j]);
-		} 
-		var originPoint = objects[i].position.clone();
-		for (var vertexIndex = 0; vertexIndex < objects[i].geometry.vertices.length; vertexIndex++)
-			{		
-			var localVertex = objects[i].geometry.vertices[vertexIndex].clone();
-			var globalVertex = localVertex.applyMatrix4( objects[i].matrix );
-			var directionVector = globalVertex.sub( objects[i].position );
-			
-			var ray = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
-			var collisionResults = ray.intersectObjects( objectsp );
-			if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ) 
-			{
-				sink(collisionResults);
-			}
-		}			
-	}
-}
 
+function collide(ship)
+{
+	var hit = false; 
+	
+	return hit;
+}
